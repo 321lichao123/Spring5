142b36d430583d3aeba46b86a69292a120be12ed
李超 <1246877006@qq.com>
2021-12-15 23:14:35 +0800

    事务的注解使用

--- day06/src/com/atguigu/serive/UserService.java
+++ day06/src/com/atguigu/serive/UserService.java
@@ -4,6 +4,8 @@ import com.atguigu.dao.UserDao;
 import com.atguigu.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @ClassName UserService
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Service;
  * @Version 1.0
  */
 @Service
+@Transactional(propagation = Propagation.REQUIRED) // 可以添加到类上也可以添加到方法上
 public class UserService {
 
     // 注入dao
@@ -20,8 +23,18 @@ public class UserService {
     private UserDao userDao;
 
     public void accountMoney() {
-        userDao.reduceMoney("Lucy");
-        int i = 12 /0;
-        userDao.addMoney("Mary");
+        //try {
+            // 第一步 开启事务
+
+            // 第二步 进行业务操作
+            userDao.reduceMoney("Lucy");
+            // 模拟异常
+            int i = 12 /0;
+            userDao.addMoney("Mary");
+            // 第三步 如果没有异常提交事务
+        //} catch (Exception e) {
+            // 第四步 如果出现异常事务回滚
+
+        //}
     }
 }
